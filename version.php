f980bf683d9c7ebcbc6b3091bda6dd3c2c03ce69
Shagun Gupta <shagung.dynamicpixel@gmail.com>
2026-06-26 17:07:41 +0530

    Update plugin version and release number

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_haccgen';
-$plugin->version = 2026021706;
-$plugin->release = '1.5';
+$plugin->version = 2026021707;
+$plugin->release = '1.6';
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = ['editor_tiny' => ANY_VERSION];
